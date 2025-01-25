c5f88ac2a06dc490876b3a0d2d0990802654478b
cmead05 <mead05cameron@gmail.com>
2025-01-25 17:05:50 -0500

    Commit test file

---
+++ test.java
@@ -0,0 +1,19 @@
+class test {
+
+    /**
+     * Does nothing
+     * @param num
+     */
+    public void cancel(int num){
+
+    }
+
+    /**
+     * Returns string that says hello to param 'name'
+     * @param name
+     * @return
+     */
+    public String hello(String name){
+        return ("Hello " + name);
+    }
+}
