d8b5cfdc1a98d8c63b0f9d52c31f9bcac3c42b41
cmead05 <mead05cameron@gmail.com>
2025-01-28 18:40:40 -0500

    Commit minor changes

--- test.java
+++ test.java
@@ -1,19 +1,8 @@
-class test {
-
-    /**
-     * Does nothing
-     * @param num
-     */
-    public void cancel(int num){
-
-    }
-
-    /**
-     * Returns string that says hello to param 'name'
-     * @param name
-     * @return
-     */
-    public String hello(String name){
-        return ("Hello " + name);
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!");
+        
     }
 }
