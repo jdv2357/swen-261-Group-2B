f04411833b26c9a91f0727e06219460fa071f8a1
cmead05 <mead05cameron@gmail.com>
2025-02-04 14:33:50 -0500

    Added utility directory, file, and methods

---
+++ petshelter/src/main/java/pet_shelter/util_code/Util.java
@@ -0,0 +1,47 @@
+public class Util {
+
+    /**
+     * Clears console screen and returns cursor to top left
+     * */
+    public static void clearConsole(){
+        System.out.print("\033[H\033[2J");
+    }
+
+    /**
+     * Puts current thread to sleep for 'ms' milliseconds
+     * @param ms
+     * */
+    public static void pauseThread(int ms){
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e){
+            System.out.println("Interruption in Util.pause_thread()");
+        }
+    }
+
+    /**
+     * Prints 'text' to console one character at a time with a 'ms' pause between characters
+     * Does not print newline character at the end of string
+     * @param ms
+     * @param text
+     * */
+    public static void bufferText(String text, int ms){
+        for(char ch : text.toCharArray()){
+            pauseThread(ms);
+            System.out.print(ch);
+        }
+    }
+
+
+    //Main method just for testing utility methods
+    public static void main(String[] args){
+
+        clearConsole();
+        bufferText("The main method in the class Util exists only to test the methods within it\na b c d e f g\n1 2 3 4 5 6 7", 75);
+        pauseThread(1000);
+        clearConsole();
+
+    }
+
+}
+
